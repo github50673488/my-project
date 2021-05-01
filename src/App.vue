fe67625829f19fd39f77cca9823de423c1636af9
liuguojun <gj.liu@sj-sol.com>
2021-05-01 14:12:23 +0900

    add bootstrap （and jquery）

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-<!--    <img src="./assets/logo.png">-->
+    <!--    <img src="./assets/logo.png">-->
+<!--    <div class="alert alert-primary" role="alert">-->
+    <!--      bootstrap is OK-->
+    <!--    </div>-->
     <router-view/>
   </div>
 </template>
@@ -12,12 +15,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/*#app {*/
+/*  font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+/*  -webkit-font-smoothing: antialiased;*/
+/*  -moz-osx-font-smoothing: grayscale;*/
+/*  text-align: center;*/
+/*  color: #2c3e50;*/
+/*  margin-top: 60px;*/
+/*}*/
 </style>
